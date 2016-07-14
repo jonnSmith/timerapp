ed52da125edc2f8f1ae1b93256a557ab5f0469de
jonnSmith <eugenpushkaroff@gmail.com>
2016-07-14 19:29:14 +0300

    copyImages

--- grunt/copy.js
+++ grunt/copy.js
@@ -35,7 +35,7 @@ module.exports = {
                 'languages/{,*/}*.json',
                 'views/{,*/}*.html',
                 'tpl/{,*/}*.html',
-                'images/{,*/}*.{webp}',
+                'images/*',
                 'fonts/*'
             ]
         }, {
